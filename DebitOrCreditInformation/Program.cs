53d5c6908e19f05c8c94c8e44c54a87b40b565e0
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 15:29:01 -0230

    Add files via upload

---
+++ DebitOrCreditInformation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebitOrCreditInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "AccountInformation.txt";
+
+            if (File.Exists(filename))
+            {
+                // call the GetInformation method and pass it the filename
+                WriteLine(filename);
+            }
+            else
+            {
+                // if file doesn't exist, print error message
+                Console.WriteLine(filename + " does not exist");
+            }
+        }
+    }
+    }
+}
